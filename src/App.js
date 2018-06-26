7d55f13efbfd83719fe497629fde5ba8a50cc31f
snkoski <shawnnkoski@gmail.com>
2018-06-26 15:20:00 -0400

    made it work :)

--- src/App.js
+++ src/App.js
@@ -10,17 +10,37 @@ class App extends Component {
 
     return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
   }
+  state = {
+    color: 'ffffff',
+  }
+
+  handleKeyUp = (e) => {
+    let x = ''
+    x += e.target.value
+    console.log(x);
+    if(x.length === 6) {
+    this.setState({
+      color: e.target.value
+    })
+  } else {
+    this.setState({
+      color: 'ffffff'
+    })
+  }
+  }
 
   render() {
+    console.log(this.invertColor(this.state.color));
     return (
-      <main
+      <main onKeyUp={this.handleKeyUp}
         style={{
-          backgroundColor: '#ffffff' /* Your code here */
+          backgroundColor: `#${this.state.color}` /* Your code here */
         }}
       >
-        <input
+        <input onKeyUp={this.handleKeyUp}
           placeholder="000000"
-          style={{ color: '#000000' /* Your code here */ }}
+          style={{ color: `${this.invertColor(this.state.color)}` /* Your code here */ }}
+
         />
       </main>
     );
